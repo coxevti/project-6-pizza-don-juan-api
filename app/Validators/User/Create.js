838f39a9384919c33e0459720e06b733ce6805d3
Valdir Coxev <valcox.russo@gmail.com>
2019-06-06 15:42:54 -0400

    Adding validator user create

---
+++ app/Validators/User/Create.js
@@ -0,0 +1,23 @@
+'use strict'
+
+class UserCreate {
+  get rules () {
+    return {
+      email: 'required|email|unique:users',
+      fullname: 'required',
+      password: 'required'
+    }
+  }
+
+  get messages () {
+    return {
+      'email.required': 'Você deve fornecer um endereço de e-mail.',
+      'email.email': 'Você deve fornecer um endereço de email válido.',
+      'email.unique': 'Este e-mail já está cadastrado.',
+      'fullname.required': 'Você deve fornecer um nome completo.',
+      'password.required': 'Você deve fornecer uma senha.'
+    }
+  }
+}
+
+module.exports = UserCreate
