db73256a6d705dfc2d973789b9344d9cb94bc41b
Valdir Coxev <valcox.russo@gmail.com>
2019-06-14 08:35:36 -0400

    Adding routes products, types and sizes

--- start/routes.js
+++ start/routes.js
@@ -18,3 +18,9 @@ const Route = use('Route')
 
 Route.post('register', 'AuthController.register').validator('Auth/Register')
 Route.post('login', 'AuthController.login')
+
+Route.group(() => {
+  Route.resource('products', 'ProductController').apiOnly()
+  Route.resource('types', 'TypeController').apiOnly()
+  Route.resource('sizes', 'SizeController').apiOnly()
+}).middleware(['auth'])
